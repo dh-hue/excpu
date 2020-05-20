e8faa13735d9e804426e980c433ddbd4d158fa46
dh-hue <54357856+dh-hue@users.noreply.github.com>
2020-05-20 17:56:18 +0530

    Update Memory.java

--- Memory.java
+++ Memory.java
@@ -1,7 +1,7 @@
 package com.company;
 import java.util.*;
 public class Memory{
-    public static final int PROGRAM_LEN_OFF = 0*F;{
+    public static final int PROGRAM_LEN_OFF = 0*F;
         Scanner n = new Scanner (System.in);
         public byte[] mem = new byte[1024*1024*3];
     }
