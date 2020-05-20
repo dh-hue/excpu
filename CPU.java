13ae60a945290caa4ad385c1dff3a6e6805f54ed
dh-hue <54357856+dh-hue@users.noreply.github.com>
2020-05-20 17:50:26 +0530

    Add files via upload

---
+++ CPU.java
@@ -0,0 +1,9 @@
+package com.company;
+import java.util.*;
+public class CPU {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int instrcounter;
+        int program_end_location;
+    }
+}
---
+++ Memory.java
@@ -0,0 +1,8 @@
+package com.company;
+import java.util.*;
+public class Memory{
+    public static final int PROGRAM_LEN_OFF = 0*F;{
+        Scanner n = new Scanner (System.in);
+        public byte[] mem = new byte[1024*1024*3];
+    }
+}
